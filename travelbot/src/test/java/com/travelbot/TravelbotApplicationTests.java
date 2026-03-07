06562cb25a1234f77353587b343d9d8f4a9d8ab2
jad bani jarid <jad.jarid99@gmail.com>
2026-03-07 23:09:53 +0100

    Initial commit

---
+++ travelbot/src/test/java/com/travelbot/TravelbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
